79927edbf8f64ae030a347bbfdd8b5c6d80606e7
pptasinski <przemyslaw.ptasinski@gmail.com>
2025-04-24 04:03:00 +0200

    [sylius-nyc] adds symfony maker bundle

--- config/bundles.php
+++ config/bundles.php
@@ -62,4 +62,5 @@ return [
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Sylius\PayPalPlugin\SyliusPayPalPlugin::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
